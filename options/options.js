6feb3f89ba23df3f7cbb60922288a2d5dbadfb1c
Crowdbotics <team@crowdbotics.com>
2023-06-01 23:31:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "booze-games-41168",
+  url: "https://booze-games-41168.botics.co",
+  api: "https://booze-games-41168.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
